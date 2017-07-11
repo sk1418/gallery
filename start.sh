119af4237bc3783f723c4cc9eda2b58c2b3b4d49
Kent@LsyHP <kent.yuan@Gmail.com>
2017-07-11 11:52:44 +0200

    add images

--- start.sh
+++ start.sh
@@ -6,10 +6,15 @@ if [[ $1 == "-d" ]]; then
 fi
 
 SRC="$HOME/Dropbox/书法/mine/"
-rsync $DRY --ignore-existing -av "${SRC}精临练笔" "store/"
-rsync $DRY --ignore-existing -av "${SRC}古诗文" "store/"
-rsync $DRY --ignore-existing -av "${SRC}闲笔" "store/"
-rsync $DRY --ignore-existing -av "${SRC}歌词" "store/"
+rsync $DRY -ignore-existing --delete-after --exclude="thumbnail/" -a "${SRC}精临练笔" "store/"
+rsync $DRY -ignore-existing --delete-after --exclude="thumbnail/" -a "${SRC}古诗文" "store/"
+rsync $DRY -ignore-existing --delete-after --exclude="thumbnail/" -a "${SRC}闲笔" "store/"
+rsync $DRY -ignore-existing --delete-after --exclude="thumbnail/" -a "${SRC}歌词" "store/"
+
+#rsync $DRY -ignore-existing --delete -av "${SRC}精临练笔" "store/"
+#rsync $DRY -ignore-existing --delete -av "${SRC}古诗文" "store/"
+#rsync $DRY -ignore-existing --delete -av "${SRC}闲笔" "store/"
+#rsync $DRY -ignore-existing --delete -av "${SRC}歌词" "store/"
 
 if [[ $1 == "-d" ]];then
 echo "============================================================================="
