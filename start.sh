02fcd22bac0a926eb12d08d43da13c72cc883db2
Kent@LsyHP <kent.yuan@Gmail.com>
2017-07-11 12:08:03 +0200

    add images

--- start.sh
+++ start.sh
@@ -6,15 +6,10 @@ if [[ $1 == "-d" ]]; then
 fi
 
 SRC="$HOME/Dropbox/书法/mine/"
-rsync $DRY -ignore-existing --delete-after --exclude="thumbnail/" -a "${SRC}精临练笔" "store/"
-rsync $DRY -ignore-existing --delete-after --exclude="thumbnail/" -a "${SRC}古诗文" "store/"
-rsync $DRY -ignore-existing --delete-after --exclude="thumbnail/" -a "${SRC}闲笔" "store/"
-rsync $DRY -ignore-existing --delete-after --exclude="thumbnail/" -a "${SRC}歌词" "store/"
-
-#rsync $DRY -ignore-existing --delete -av "${SRC}精临练笔" "store/"
-#rsync $DRY -ignore-existing --delete -av "${SRC}古诗文" "store/"
-#rsync $DRY -ignore-existing --delete -av "${SRC}闲笔" "store/"
-#rsync $DRY -ignore-existing --delete -av "${SRC}歌词" "store/"
+rsync $DRY --ignore-existing --delete --exclude="thumbnail/" -av "${SRC}精临练笔" "store/"
+rsync $DRY --ignore-existing --delete --exclude="thumbnail/" -av "${SRC}古诗文" "store/"
+rsync $DRY --ignore-existing --delete --exclude="thumbnail/" -av "${SRC}闲笔" "store/"
+rsync $DRY --ignore-existing --delete --exclude="thumbnail/" -av "${SRC}歌词" "store/"
 
 if [[ $1 == "-d" ]];then
 echo "============================================================================="
